24b5e4f58057dda12e26d0d1b0e6589b14ebb021
Vladut Mocanu <vladut.mocanu@webamboos.com>
2024-10-06 01:54:25 +0300

    added functions to check the number of words, number of each chars appearance.

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("hello world")
+def main():
+    book_url = "books/frankenstein.txt"
+    book_text = get_book_text(book_url)
+    total_book_words = count_book_words(book_text)
+    print(f"The book has a total of {total_book_words} words.")  
+    print(f"The book has the following chars count: {characters_dictionary(book_text)}")
+
+
+def get_book_text(path):
+    with open(path) as book:
+        book_content = book.read()
+        return book_content
+
+def count_book_words(book_text):
+    words_list = book_text.split()
+    words_total = 0
+    for word in words_list:
+        words_total += 1 
+    return words_total
+
+def characters_dictionary(book_text):
+    words_list = book_text.split()
+    chars_list = []
+    chars_dictionary = {}
+    for word in words_list:
+        for char in word:
+            chars_list.append(char.lower())
+    chars_set = set(chars_list)
+    chars_list_no_duplicates = list(chars_set)
+    
+    for char in chars_list_no_duplicates:
+        chars_dictionary[char] = 0
+        for elem in chars_list:
+            if char == elem:
+                chars_dictionary[char] += 1
+    return chars_dictionary
+            
+
+
+
+            
+
+
+main()
