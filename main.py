aa896c32c9e248b1f3d41f39bb6fbfbd60cb5411
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2024-10-06 18:41:35 +0300

    Added different functions to extract data from the book.

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     book_url = "books/frankenstein.txt"
     book_text = get_book_text(book_url)
     total_book_words = count_book_words(book_text)
-    print(f"The book has a total of {total_book_words} words.")  
-    print(f"The book has the following chars count: {characters_dictionary(book_text)}")
-
+    chars_appearances = characters_dictionary(book_text)
+    list_chars_appearances = create_list_of_dicts(chars_appearances)
+    print(f"--- Begin report of {book_url} ---")
+    print(f"{total_book_words} words found in document.")
+    print("\n")
+    display_all_occurences(list_chars_appearances)
+    print('--- End report ---')
+    
 
 def get_book_text(path):
     with open(path) as book:
@@ -17,23 +22,34 @@ def count_book_words(book_text):
     return words_total
 
 def characters_dictionary(book_text):
-    words_list = book_text.split()
-    chars_list = []
-    chars_dictionary = {}
-    for word in words_list:
-        for char in word:
-            chars_list.append(char.lower())
-    chars_set = set(chars_list)
-    chars_list_no_duplicates = list(chars_set)
-    
-    for char in chars_list_no_duplicates:
-        chars_dictionary[char] = 0
-        for elem in chars_list:
-            if char == elem:
-                chars_dictionary[char] += 1
-    return chars_dictionary
-            
-            
+    chars_dict = {}
+    for char in book_text:
+        lowercase_char = char.lower()
+        if lowercase_char in chars_dict:
+            chars_dict[lowercase_char] += 1
+        else:
+            chars_dict[lowercase_char] = 1
+    return chars_dict
+
+
+def create_list_of_dicts(dict):
+    dicts_list = []
+    for letter in dict:
+        if letter.isalpha():
+            current_new_dict={}
+            current_new_dict['letter'] = letter
+            current_new_dict['number'] = dict[letter]
+            dicts_list.append(current_new_dict)
+    return dicts_list
+
+
+def sort_after(dict):
+    return dict['number']
+
+def display_all_occurences(list):
+    list.sort(reverse=True, key=sort_after)
+    for dict in list:
+        print(f'The {dict['letter']} character was found {dict['number']} times')
 
 
 main()
